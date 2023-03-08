2afcee02b80d6f78ae4a37c54153fc881fa14f08
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-03-08 19:49:00 +0000

    encode and decode wrapper with size

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use std::{error::Error, fmt};
+use std::{error::Error, fmt, mem::size_of};
 
 /// Ensure the step size is valid
 /// Step must be [1, 8] and a factor of 8
 macro_rules! debug_assert_step_size {
     ( $step:ident ) => {
-        debug_assert!(0 < $step && $step < 8);
-        debug_assert_eq!(8 % $step, 0);
+        debug_assert!(0 < $step && $step < u8::BITS as usize);
+        debug_assert_eq!(u8::BITS as usize % $step, 0);
     };
 }
 
@@ -33,7 +33,11 @@ impl Error for BufferSizeError {}
 /// # Ok(())
 /// # }
 /// ```
-pub fn encode_raw(buffer: &mut [u8], data: &[u8], step: usize) -> Result<(), BufferSizeError> {
+pub fn encode_raw<'a>(
+    buffer: &'a mut [u8],
+    data: &[u8],
+    step: usize,
+) -> Result<&'a mut [u8], BufferSizeError> {
     debug_assert_step_size!(step);
     if buffer.len() < (data.len() * step) {
         return Err(BufferSizeError {
@@ -41,10 +45,11 @@ pub fn encode_raw(buffer: &mut [u8], data: &[u8], step: usize) -> Result<(), Buf
         });
     }
 
+    let space = u8::BITS as usize / step;
     let mut it = buffer.iter_mut();
     for byte in data {
         let mut bit_read: u8 = 0;
-        for slot in (&mut it).take(8 / step) {
+        for slot in (&mut it).take(space) {
             for bit_write in 0..step {
                 let insert = (byte & (1 << bit_read)) >> bit_read;
                 let old = (*slot) & (!(1 << bit_write));
@@ -53,10 +58,11 @@ pub fn encode_raw(buffer: &mut [u8], data: &[u8], step: usize) -> Result<(), Buf
             }
         }
     }
-    Ok(())
+    Ok(&mut buffer[data.len() * space..])
 }
 
 fn decode_byte(buffer: &[u8], step: usize) -> Option<u8> {
+    debug_assert_step_size!(step);
     let mut current: u8 = 0;
     let mut bit = 0;
     for slot in buffer {
@@ -87,15 +93,46 @@ fn decode_byte(buffer: &[u8], step: usize) -> Option<u8> {
 /// # Ok(())
 /// # }
 /// ```
-pub fn decode_raw(buffer: &[u8], step: usize) -> Vec<u8> {
-    let index_step = 8 / step;
+pub fn decode_raw<'a>(buffer: &'a [u8], size: usize, step: usize) -> (&'a [u8], Vec<u8>) {
+    debug_assert_step_size!(step);
+    let index_step = u8::BITS as usize / step;
     let mut out = Vec::<u8>::new();
-    for index in (0..buffer.len()).step_by(index_step) {
+    for index in (0..buffer.len()).step_by(index_step).take(size) {
         if let Some(byte) = decode_byte(&buffer[index..index + index_step], step) {
             out.push(byte);
         } else {
             break;
         }
     }
-    out
+    (&buffer[(size * index_step)..], out)
+}
+
+pub fn encode<'a>(
+    buffer: &'a mut [u8],
+    data: &[u8],
+    step: usize,
+) -> Result<&'a mut [u8], BufferSizeError> {
+    debug_assert_step_size!(step);
+    let size = data.len().to_be_bytes();
+    let buffer = encode_raw(buffer, &size, step)?;
+    Ok(encode_raw(buffer, data, step)?)
+}
+
+pub fn decode(buffer: &[u8], step: usize) -> Result<Vec<u8>, BufferSizeError> {
+    debug_assert_step_size!(step);
+    let (buffer, size) = decode_raw(buffer, size_of::<usize>(), step);
+    if size.len() != size_of::<usize>() {
+        return Err(BufferSizeError {
+            missing: size.len(),
+        });
+    }
+    let size = usize::from_be_bytes(size.try_into().unwrap());
+    let (_, data) = decode_raw(buffer, size, step);
+    if data.len() != size {
+        Err(BufferSizeError {
+            missing: size - data.len(),
+        })
+    } else {
+        Ok(data)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::{error::Error, fs::File, io::BufReader, str};
 const STEGO: usize = 2;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    assert_eq!(8usize % STEGO, 0);
-
     let file = File::open("img/Classic_Rainbow_Flag.png")?;
     let file = BufReader::new(file);
 
@@ -14,14 +12,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let info = reader.next_frame(&mut buf)?;
     buf.truncate(info.buffer_size());
 
-    let mut buf = vec![0; 100];
-
     let msg = "Hello World";
-    stegosaurus::encode_raw(&mut buf, msg.as_bytes(), STEGO)?;
+    stegosaurus::encode(&mut buf, msg.as_bytes(), STEGO)?;
 
-    let out = stegosaurus::decode_raw(&buf, STEGO);
-    println!("Raw: {:?}", &out[..msg.len()]);
-    let message = str::from_utf8(&out[..msg.len()]).unwrap();
+    let out = stegosaurus::decode(&buf, STEGO)?;
+    let message = str::from_utf8(&out).unwrap();
     println!("Output: {message}");
 
     Ok(())
